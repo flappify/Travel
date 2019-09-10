01521f30c284d4f918fe6bcb4729fda0333d9e87
saahilshaikh <saahilshaikhrocks@gmail.com>
2019-09-10 12:20:26 +0530

    FILE UPLOAD

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/**Sticky Navbar **/
+window.onscroll = function () {
+    myFunction()
+};
+var navbar = document.getElementById("myNav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+/**wow js */
+new WOW().init();
+
+/**slideshow */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 7000);
+}
